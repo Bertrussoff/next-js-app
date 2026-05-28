6fd02534e979bcad1a619a3214995b5adb75b515
Bertram Russell <bertramrussell@tests-MacBook-Pro.local>
2026-05-28 17:13:48 +0530

    New changes

--- app/counter.test.tsx
+++ app/counter.test.tsx
@@ -10,3 +10,6 @@ it("App Router: Works with Client Components (React State)", () => {
   fireEvent.click(screen.getByRole("button"));
   expect(screen.getByRole("heading")).toHaveTextContent("1");
 });
+    expect(screen.getByRole("heading")).toHaveTextContent("1");
+  });
+});
